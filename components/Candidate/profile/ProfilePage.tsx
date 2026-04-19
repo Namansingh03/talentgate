e7f146b8ee47728e7c4a0992e9a34aaae07275d5
Namansingh03 <singhnaman6789@gmail.com>
2026-04-19 13:10:52 +0000

    added profile skeletons

--- components/Candidate/profile/ProfilePage.tsx
+++ components/Candidate/profile/ProfilePage.tsx
@@ -6,18 +6,24 @@ import ExperienceCard from "./ExperienceCard";
 import EducationCard from "./EducationCard";
 import AboutCard from "./AboutCard";
 import { UserWithProfile } from "@/types/prismaTypes";
+import { Suspense } from "react";
+import ProfileHeaderSkeleton from "./ProfileSkeleton/ProfileHeaderSkeleton";
+import AboutSkeleton from "./ProfileSkeleton/AboutSkeleton";
+import ExperienceSkeleton from "./ProfileSkeleton/ExperienceSkeleton";
 
 export default function ProfilePage({ user }: { user: UserWithProfile }) {
   return (
     <div className="w-full bg-blue-500 relative">
-      <ProfileHeader
-        AvatarImageUrl={user?.image}
-        displayName={user?.name}
-        headline={user?.candidateProfile?.headline}
-        isAvailable={user?.candidateProfile?.isOpenToWork}
-        location={user?.candidateProfile?.location}
-        username={user?.username}
-      />
+      <Suspense fallback={<ProfileHeaderSkeleton />}>
+        <ProfileHeader
+          AvatarImageUrl={user?.image}
+          displayName={user?.name}
+          headline={user?.candidateProfile?.headline}
+          isAvailable={user?.candidateProfile?.isOpenToWork}
+          location={user?.candidateProfile?.location}
+          username={user?.username}
+        />
+      </Suspense>
 
       {/* Main Content Wrapper */}
       <div className="w-full flex justify-center absolute top-60 px-10 gap-x-10 ">
@@ -32,9 +38,13 @@ export default function ProfilePage({ user }: { user: UserWithProfile }) {
         <div className="w-full max-w-2xl flex gap-10 items-start">
           {/* Main Content */}
           <div className="flex-1 flex flex-col gap-5 rounded-2xl">
-            <AboutCard about={user.candidateProfile?.about} />
+            <Suspense fallback={<AboutSkeleton />}>
+              <AboutCard about={user.candidateProfile?.about} />
+            </Suspense>
             <ExperienceCard experiences={user.candidateProfile?.experience} />
+            <Suspense fallback={<ExperienceSkeleton />}></Suspense>
             <EducationCard education={user.candidateProfile?.education} />
+            <Suspense fallback={<EducationCard />}></Suspense>
           </div>
         </div>
       </div>
--- components/Candidate/profile/ProfileSidebar.tsx
+++ components/Candidate/profile/ProfileSidebar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { Separator } from "../../ui/separator";
 import ContactCard from "./ContactCard";
 import { CardWrapper } from "../../ui/CardWrapper";
 import SkillsCard from "./SkillsCard";
+import SkillsSkeleton from "./ProfileSkeleton/SkillsSkeleton";
 
 interface ProfileSidebarProps {
   bio?: string | null;
@@ -24,23 +25,25 @@ export default function ProfileSidebar({
   skills,
 }: ProfileSidebarProps) {
   return (
-    <CardWrapper className="h-fit sticky flex flex-col gap-y-5 max-w-1/3">
-      <div id="personal info">
-        <h1 className="text-muted-foreground flex justify-between">
-          Personal information
-          <span className=" text-blue-500 text-xs">Edit</span>
-        </h1>
-        <p className="mt-2 text-sm">{bio}</p>
-      </div>
-      <Separator />
-      <ContactCard
-        githubUrl={githubUrl}
-        linkedinUrl={linkedinUrl}
-        portfolioUrl={portfolioUrl}
-        resumeUrl={resumeUrl}
-      />
-      <Separator />
-      <SkillsCard skills={skills} />
-    </CardWrapper>
+    <Suspense fallback={<SkillsSkeleton />}>
+      <CardWrapper className="h-fit sticky flex flex-col gap-y-5 max-w-1/3">
+        <div id="personal info">
+          <h1 className="text-muted-foreground flex justify-between">
+            Personal information
+            <span className=" text-blue-500 text-xs">Edit</span>
+          </h1>
+          <p className="mt-2 text-sm">{bio}</p>
+        </div>
+        <Separator />
+        <ContactCard
+          githubUrl={githubUrl}
+          linkedinUrl={linkedinUrl}
+          portfolioUrl={portfolioUrl}
+          resumeUrl={resumeUrl}
+        />
+        <Separator />
+        <SkillsCard skills={skills} />
+      </CardWrapper>
+    </Suspense>
   );
 }
